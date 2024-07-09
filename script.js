f8bf051a285aa1bffd06d18d0038414953c3bcfc
SerinaUeno <sfx1225@gmail.com>
2024-07-09 16:26:48 +0900

    update html and css and script

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // ハンバーガーメニューの動作
+    const menuToggle = document.getElementById('menu-toggle');
+    const navList = document.getElementById('nav-list');
+    
+    menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+
     // アコーディオンの動作
     const accordionItems = document.querySelectorAll('.accordion-item h3');
     accordionItems.forEach(item => {
